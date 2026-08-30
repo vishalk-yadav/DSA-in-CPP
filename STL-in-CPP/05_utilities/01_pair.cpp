f37b33b5411bba5a86c3737c37f41a67a18ab527
Vishal Kumar Yadav <vishalkryadav.work@gmail.com>
2026-08-31 00:12:21 +0530

    Add STL in CPP

--- STL-in-CPP/05_utilities/01_pair.cpp
+++ STL-in-CPP/05_utilities/01_pair.cpp
@@ -1,15 +1,14 @@
-#include <utility>
 #include <iostream>
 #include <string>
+#include <utility>
 using namespace std;
 
 int main() {
-    pair<string, int> p = {"Vishal", 95};
+  pair<string, int> p = {"Vishal", 95};
 
-    cout << p.first << " " << p.second << '\n';
+  cout << p.first << " " << p.second << '\n';
 
-    pair<int, pair<int, int>> nested = {1, {2, 3}};
-    cout << nested.first << ' '
-         << nested.second.first << ' '
-         << nested.second.second << '\n';
+  pair<int, pair<int, int>> nested = {1, {2, 3}};
+  cout << nested.first << ' ' << nested.second.first << ' '
+       << nested.second.second << '\n';
 }
